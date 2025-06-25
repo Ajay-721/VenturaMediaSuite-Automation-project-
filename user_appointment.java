f145c01daa6b42832b8833d476ee798a5b6ef37d
Ajay Devan M <ajaydevan721@gmail.com>
2025-06-24 20:54:43 -0700

    Update user_appointment.java

--- user_appointment.java
+++ user_appointment.java
@@ -28,4 +28,29 @@ public class user_appointment {
 		
 		
   }
+	@Test
+  public void view_appointment() {
+	  WebDriver ab=new ChromeDriver();
+		ab.get("https://venturamedsuite.infomedica.in/");
+		ab.manage().window().maximize();
+		ab.findElement(By.linkText("Sign In")).click();
+		ab.findElement(By.id("form3Example3")).sendKeys("aradhya@gmail.com");
+		ab.findElement(By.id("form3Example4")).sendKeys("123");
+		ab.findElement(By.xpath("/html/body/section/div/div/div/div/form/div[5]/button")).click();
+		ab.findElement(By.xpath("/html/body/div[1]/header/div/div/div/div[2]/nav/div[2]/ul/li[3]/a")).click();
+		ab.findElement(By.xpath("/html/body/div[1]/header/div/div/div/div[2]/nav/div[2]/ul/li[3]/ul/li[2]/a")).click();
+		JavascriptExecutor cd = (JavascriptExecutor) ab;
+		  for (int i = 0; i < 10; i++) {
+		  cd.executeScript("window.scrollBy(0, 200)");
+
+		   try {
+		    Thread.sleep(1000);
+		   } catch (InterruptedException e) {
+		    e.printStackTrace();
+		   }
+		
+		
+		
+}
+  }
 }
